0f26d93336247893940a777f16f969c8650a0fad
Taras Sivaiev <99833367+Lambda228@users.noreply.github.com>
2022-06-15 19:15:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// let playlist = [{author: 'LED ZEPPELIN',
+// song: 'STAIRWAY TO HEAVEN'}, {author: 'QUEEN', song: 'BOHEMIAN RHAPSODY'}, { author: 'LYNYRD SKYNYRD', song: 'FREE BIRD'}, {author: 'DEEP PURPLE', song: 'SMOKE ON THE WATER'}, {author: 'JIMI HENDRIX', song: 'ALL ALONG THE WATCHTOWER'}, {author: 'AC/DC', song: 'BACK IN BLACK'}, {author: 'QUEEN', song: 'WE WILL ROCK YOU'}, {author: 'METALLICA', song: 'ENTER SANDMAN'}];
+// let songlist = [];
+// let list = document.getElementById("list");
+
+
+// playlist.forEach(function(element) {
+//   songlist.push("<li>" + element.author + " - " + element.song + "</li>");
+// });
+
+// list.innerHTML = songlist.join('');
+
+
+
+// let openButton = document.querySelector('.open');
+ 
+function popUpOpen() {
+    let popUp = document.getElementById('popUp');
+    popUp.style.display = "block";
+    
+};
+
