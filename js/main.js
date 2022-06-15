b18ffe0544afb03afd24f98b40b17541252e395c
Taras Sivaiev <99833367+Lambda228@users.noreply.github.com>
2022-06-15 19:42:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-// let playlist = [{author: 'LED ZEPPELIN',
-// song: 'STAIRWAY TO HEAVEN'}, {author: 'QUEEN', song: 'BOHEMIAN RHAPSODY'}, { author: 'LYNYRD SKYNYRD', song: 'FREE BIRD'}, {author: 'DEEP PURPLE', song: 'SMOKE ON THE WATER'}, {author: 'JIMI HENDRIX', song: 'ALL ALONG THE WATCHTOWER'}, {author: 'AC/DC', song: 'BACK IN BLACK'}, {author: 'QUEEN', song: 'WE WILL ROCK YOU'}, {author: 'METALLICA', song: 'ENTER SANDMAN'}];
-// let songlist = [];
-// let list = document.getElementById("list");
+ let playlist = [{author: 'LED ZEPPELIN',
+ song: 'STAIRWAY TO HEAVEN'}, {author: 'QUEEN', song: 'BOHEMIAN RHAPSODY'}, { author: 'LYNYRD SKYNYRD', song: 'FREE BIRD'}, {author: 'DEEP PURPLE', song: 'SMOKE ON THE WATER'}, {author: 'JIMI HENDRIX', song: 'ALL ALONG THE WATCHTOWER'}, {author: 'AC/DC', song: 'BACK IN BLACK'}, {author: 'QUEEN', song: 'WE WILL ROCK YOU'}, {author: 'METALLICA', song: 'ENTER SANDMAN'}];
+ let songlist = [];
+ let list = document.getElementById("list");
 
 
-// playlist.forEach(function(element) {
-//   songlist.push("<li>" + element.author + " - " + element.song + "</li>");
-// });
+ playlist.forEach(function(element) {
+   songlist.push("<li>" + element.author + " - " + element.song + "</li>");
+ });
 
-// list.innerHTML = songlist.join('');
+list.innerHTML = songlist.join('');
 
 
 
@@ -17,6 +17,12 @@
 function popUpOpen() {
     let popUp = document.getElementById('popUp');
     popUp.style.display = "block";
+    
+};
+
+function popUpClose() {
+    let popUp = document.getElementById('popUp');
+    popUp.style.display = "none";
     
 };
 
